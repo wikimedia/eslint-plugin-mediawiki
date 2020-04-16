57e636684f84afca8eb03ae8598a9e1981232267
Roan Kattouw <roan.kattouw@gmail.com>
2020-04-15 17:36:59 -0700

    valid-package-file-require: Check if require() arg looks like a path
    If require() is called with an argument that doesn't look like a path,
    it's probably a module name, and we should leave it alone.
    require('foo') should be allowed, even if 'foo.js' exists.
    
    Only apply this rule if the require() argument looks like a file path,
    meaning it either starts with ./ or ../, or ends with .js or .json.
    
    Fixes #17

--- tests/valid-package-file-require.js
+++ tests/valid-package-file-require.js
@@ -12,6 +12,18 @@ ruleTester.run( 'valid-package-file-require', rule, {
 			filename: testFileName
 		},
 		{
+			code: 'var foo = require( \'./quux.json\' );',
+			filename: testFileName
+		},
+		{
+			code: 'var bar = require( \'../valid-package-file-require.js\' );',
+			filename: testFileName
+		},
+		{
+			code: 'var foo = require( \'foo\' );',
+			filename: testFileName
+		},
+		{
 			code: 'var bar = require( \'bar\' );',
 			filename: testFileName
 		}
@@ -27,6 +39,20 @@ ruleTester.run( 'valid-package-file-require', rule, {
 		},
 		{
 			code: 'var foo = require( \'foo.js\' );',
+			filename: testFileName,
+			errors: [
+				{ message: 'bad resource loader package file path' }
+			]
+		},
+		{
+			code: 'var foo = require( \'./quux\' );',
+			filename: testFileName,
+			errors: [
+				{ message: 'bad resource loader package file path' }
+			]
+		},
+		{
+			code: 'var foo = require( \'quux.json\' );',
 			filename: testFileName,
 			errors: [
 				{ message: 'bad resource loader package file path' }
