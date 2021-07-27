3ac86ca5669b2c67fc5f799b7264b15ff056573d
DannyS712 <46829944+DannyS712@users.noreply.github.com>
2021-07-27 10:03:21 -0700

    Fix ESLint failures in vue-exports-component-directive

--- src/rules/vue-exports-component-directive.js
+++ src/rules/vue-exports-component-directive.js
@@ -28,36 +28,36 @@ module.exports = {
 			// No rules
 			return {};
 		}
-		
+
 		// Must be a Vue file, look for `module.exports =`
 		return {
 			AssignmentExpression: ( node ) => {
 				// Early return if its not `module.exports =`
-				if ( node.operator !== '='
-				    || node.left.type !== 'MemberExpression'
-				    || node.left.object.type !== 'Identifier'
-				    || node.left.object.name !== 'module'
-				    || node.left.property.type !== 'Identifier'
-				    || node.left.property.name !== 'exports'
+				if ( node.operator !== '=' ||
+					node.left.type !== 'MemberExpression' ||
+					node.left.object.type !== 'Identifier' ||
+					node.left.object.name !== 'module' ||
+					node.left.property.type !== 'Identifier' ||
+					node.left.property.name !== 'exports' ||
 				) {
 					return;
 				}
-				
+
 				// Get all the comments that match the directive, the same way that
 				// eslint-plugin-vue does
 				const commentTokens = context.getSourceCode()
 					.getAllComments()
-					.filter((comment) => /@vue\/component/g.test(comment.value));
+					.filter( ( comment ) => /@vue\/component/g.test( comment.value ) );
 				// Check for a comment on the correct line, the same way eslint-plugin-vue
 				// does to determine if it should run.
 				const hasDirectiveComment = commentTokens.some(
-					(comment) => comment.loc.end.line === node.loc.start.line - 1
+					( comment ) => comment.loc.end.line === node.loc.start.line - 1
 				);
 				// If there is such a comment, everything is fine, and return
 				if ( hasDirectiveComment ) {
 					return;
 				}
-				
+
 				// Complain about missing directive
 				context.report( {
 					node,
