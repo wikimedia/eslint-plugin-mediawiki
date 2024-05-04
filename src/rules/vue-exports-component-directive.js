a6ed59f9c657972f13c404d90a57e42be5f5c439
Roan Kattouw <roan.kattouw@gmail.com>
2024-05-04 04:11:34 -0700

    Rule fix: vue-exports-component-directive: Make fixable (#102)
    - If module.exports is set to an object literal, wrap it in a call to
      defineComponent()
    - If the defineComponent variable is not already defined, import it:
        - If there is already a `const { ... } = require( 'vue' );` block,
          add it to that
        - If not, add `const { defineComponent } = require( 'vue' );` at the
          top of the file

--- src/rules/vue-exports-component-directive.js
+++ src/rules/vue-exports-component-directive.js
@@ -14,6 +14,7 @@ module.exports = {
 		docs: {
 			description: 'Require `defineComponent()` calls or `// @vue/component` directives to trigger eslint-plugin-vue rules'
 		},
+		fixable: 'code',
 		schema: [],
 		messages: {
 			'missing-defineComponent': 'Exported component definitions should be wrapped in `defineComponent()`, or have a `// @vue/component` comment above them.'
@@ -80,11 +81,58 @@ module.exports = {
 					return;
 				}
 
-				// Complain about missing defineComponent()
-				context.report( {
-					node,
-					messageId: 'missing-defineComponent'
-				} );
+				// If the assignedValue is an object literal, this rule is fixable
+				if ( assignedValue.type === 'ObjectExpression' ) {
+					context.report( {
+						node,
+						messageId: 'missing-defineComponent',
+						*fix( fixer ) {
+							// Wrap assignedValue in defineComponent( ... )
+							yield fixer.insertTextBefore( assignedValue, 'defineComponent( ' );
+							yield fixer.insertTextAfter( assignedValue, ' )' );
+
+							// Check whether the defineComponent variable is already defined
+							const scope = context.getSourceCode().getScope( assignedValue );
+							if ( !scope.variables.some( ( v ) => v.name === 'defineComponent' ) ) {
+								// defineComponent is not defined
+
+								// Check if there is already a line that looks like
+								// const { ... } = require( 'vue' );
+								// eslint-disable-next-line es-x/no-array-prototype-flat
+								const varDefs = scope.variables.flatMap( ( v ) => v.defs );
+								const existingVar = varDefs.find( ( d ) =>
+									d.node.type === 'VariableDeclarator' &&
+									d.node.id.type === 'ObjectPattern' &&
+									d.node.init.type === 'CallExpression' &&
+									d.node.init.callee.type === 'Identifier' &&
+									d.node.init.callee.name === 'require' &&
+									d.node.init.arguments.length === 1 &&
+									d.node.init.arguments[ 0 ].type === 'Literal' &&
+									d.node.init.arguments[ 0 ].value === 'vue'
+								);
+
+								if ( existingVar ) {
+									// If there is, add defineComponent to it
+									const objectPattern = existingVar.node.id;
+									const lastVar = objectPattern.properties[
+										objectPattern.properties.length - 1
+									];
+									yield fixer.insertTextAfter( lastVar, ', defineComponent' );
+								} else {
+									// If there isn't, add a new one at the top
+									const firstNode = scope.block.body[ 0 ];
+									yield fixer.insertTextBefore( firstNode, "const { defineComponent } = require( 'vue' );\n" );
+								}
+							}
+						}
+					} );
+				} else {
+					// Otherwise, complain but don't make it fixable
+					context.report( {
+						node,
+						messageId: 'missing-defineComponent'
+					} );
+				}
 			}
 		};
 	}
