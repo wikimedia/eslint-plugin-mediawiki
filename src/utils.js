98ad2ea260dd5525ea2de732e210e410bd6788e3
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-03-19 10:24:26 +0900

    oops

--- src/utils.js
+++ src/utils.js
@@ -38,7 +38,7 @@ function requiresCommentList( context, node ) {
 		return false;
 	}
 
-	const sourceCode = ( content.sourceCode ?? context.getSourceCode() );
+	const sourceCode = ( context.sourceCode ?? context.getSourceCode() );
 	// Don't modify `node` so the correct error source is highlighted
 	let checkNode = node,
 		prevNode = node,
